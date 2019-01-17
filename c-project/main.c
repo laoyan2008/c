456d5bfec1f89d365d754d5491db56c33cb4dd86
yy <1019624863@qq.com>
2019-01-17 08:44:58 +0800

    first

---
+++ c-project/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+char arr[1024] int index = 0;
+
+int main()
+{
+    printf("欢迎使用敏感字符管理系统\n");
+    while (1)
+    {
+
+        printf("1---添加一个敏感字符\n");
+        printf("2---删除最后一个敏感字符\n");
+        printf("3---查看所有敏感字符\n");
+        printf("4---替换敏感字符\n");
+        printf("5---退出\n");
+
+        printf("请输入要使用的功能\n");
+
+        int code;
+        scanf("%d",&code);
+        if(code==1)
+        {
+            printf("请输入要添加的敏感字符");
+            char m;
+            scanf("%")
+
+
+        }
+        if(code==2)
+        {
+
+        }
+        if(code==3)
+        {
+
+        }
+        if(code==4)
+        {
+
+        }
+        if(code==5)
+        {
+            break
+        }
+    }
+
+    return 0;
+}
